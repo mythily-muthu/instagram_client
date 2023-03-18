763a16c97c486d34282bbea0d7ad114bf2c14329
mythily-muthu <mythilimuthu18@gmail.com>
2023-03-18 22:41:52 +0530

    Feat: added flexbetween component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { createTheme } from "@mui/system";
+import { themeSettings } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   return (
     <div className="App">
-      <h2>hello</h2>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/widgets.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const widgets = () => {
-  return <div>widgets</div>;
-};
-
-export default widgets;
