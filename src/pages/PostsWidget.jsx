2a067e630eea07af3cd921d4e8994aec785fb934
mythily-muthu <mythilimuthu18@gmail.com>
2023-03-21 17:00:59 +0530

    Feat: added posts widget

--- src/pages/PostsWidget.jsx
+++ src/pages/PostsWidget.jsx
@@ -4,8 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
 import PostWidget from "./PostWidget";
 
-const PostsWidget = ({ userId, isProfile }) => {
-  console.log("isProfile:", isProfile);
+const PostsWidget = ({ userId, isProfile = false }) => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts);
   const token = useSelector((state) => state.token);
@@ -16,10 +15,10 @@ const PostsWidget = ({ userId, isProfile }) => {
       headers: { Authorization: `Bearer ${token}` },
     });
     const posts = response.data;
+    console.log("posts:", posts);
     dispatch(setPosts({ posts }));
   };
 
-  // ALL POSTS OF A SINGLE USER
   const getUserPosts = async () => {
     const response = await axios.get(
       `http://localhost:7000/posts/${userId}/posts`,
@@ -28,6 +27,7 @@ const PostsWidget = ({ userId, isProfile }) => {
       }
     );
     let posts = response.data;
+
     dispatch(setPosts({ posts }));
   };
 
@@ -38,10 +38,9 @@ const PostsWidget = ({ userId, isProfile }) => {
       getPosts();
     }
   }, [userId, isProfile, token]);
-
   return (
     <>
-      {posts.map(
+      {/* {posts.map(
         ({
           _id,
           userId,
@@ -67,7 +66,7 @@ const PostsWidget = ({ userId, isProfile }) => {
             comments={comments}
           />
         )
-      )}
+      )} */}
     </>
   );
 };
